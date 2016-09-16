3d4b7d8f739da7517714b0ad53fab72ab32b1ef3
kmaimn <kmai.mn@gmail.com>
2016-09-16 17:13:05 -0500

    added env

--- server/routes/foodRoutes.js
+++ server/routes/foodRoutes.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
-var connectionString = 'postgres://localhost:5432/recipes';
+var connectionString = '';
+
+//If we are running on Heroku, use the remote database (with SSL)
+if (process.env.DATABASE_URL != undefined) {
+  connectionString = process.env.DATABASE_URL + '?ssl=true';
+} else {
+  // running locally, use our local database instead
+  connectionString = 'postgres://localhost:5432/recipes';
+}
 
 //GET current ingredients;
 router.get('/', function (req, res) {
